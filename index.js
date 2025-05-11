1545283851d1fb86e696f3f7e13f6f09068be249
Aliabbas Bhaisaheb <162595663+aliabbas222004@users.noreply.github.com>
2025-05-11 08:14:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const passport = require('passport');
 const cors=require('cors');
 const app=express();
-const port=process.env.PORT;
+const port=process.env.PORT || 5000;
 const { OpenAI } = require('openai');
 
 app.use(session({
@@ -20,7 +20,10 @@ app.use(passport.session());
 
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: '*', 
+  credentials: true,
+}))
 
 app.use('/auth',require('./routes/authRoutes'))
 app.use('/user',require('./routes/user'))
